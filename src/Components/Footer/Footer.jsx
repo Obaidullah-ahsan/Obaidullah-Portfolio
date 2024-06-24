581a45d65049d88b6e58ff428780cd16bbcb825a
Obaidullah-ahsan <abir60303@gmail.com>
2024-06-24 18:56:59 +0600

    added portfolio footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import { FaSquareGithub, FaFacebook, FaLinkedin } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <div className="flex justify-between mx-4 md:mx-32 mb-10">
+    <div className="flex flex-col md:flex-row justify-between mx-4 md:mx-32 mb-10 space-y-2 md:space-y-0">
       <h4 className="bg-gradient-to-r from-[#8D61D5] to-[#19A8C7] inline-block text-transparent bg-clip-text">
         Feel free to contact me
       </h4>
